c71f95b8e106232bc6c2a369704186f3a6f70b9f
@p_craft <ef81sp@gmail.com>
2021-10-15 01:34:19 +0900

    :shirt:

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   configureWebpack: {
-    devtool: 'source-map',
+    devtool: "source-map",
   },
   pages: {
     index: {
-      entry: 'src/main.ts',
-      title: 'DiaGen | ダイヤグラム作成サポートツール',
+      entry: "src/main.ts",
+      title: "DiaGen | ダイヤグラム作成サポートツール",
     },
   },
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/a-train-supporter/' : '/',
+    process.env.NODE_ENV === "production" ? "/a-train-supporter/" : "/",
 };
